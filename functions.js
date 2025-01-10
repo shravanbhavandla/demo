cf645ac87441adcbc6449c21a16f9229b6d716b3
shravan bhavandla <shravanbhavandla@gmail.com>
2025-01-10 13:31:08 +0530

    msg

--- functions.js
+++ functions.js
@@ -1,63 +1,107 @@
-// // // // // // // function isPalindrome(str)
+// // // // // // // // // // function isPalindrome(str)
+// // // // // // // // // // {
+// // // // // // // // // //     let revStr = "";
+// // // // // // // // // //     for(let i =str.length-1;i>=0;i--){
+// // // // // // // // // //         revStr += str[i];
+// // // // // // // // // //     }
+// // // // // // // // // //     if(str ==revStr){
+// // // // // // // // // //         console.log("Palindrome");
+// // // // // // // // // //     }else{
+// // // // // // // // // //         console.log("Not a palindrome");
+// // // // // // // // // //     }
+// // // // // // // // // // }
+
+// // // // // // // // // // isPalindrome("sir");
+// // // // // // // // // // isPlaindrome("madam");
+// // // // // // // // // // isPalindrome("malayam");
+
+
+// // // // // // // // // function onemoretime(){
+// // // // // // // // //     console.log("not undertood tell me one more time");
+
+// // // // // // // // // }
+// // // // // // // // // // ananymous function
+// // // // // // // // // function(){
+
+// // // // // // // // // }
+
+// // // // // // // // //function  expression 
+// // // // // // // // let  x =function(){
+// // // // // // // //     console.log("Function expression";)
+// // // // // // // // }
+
+// // // // // // // // !iife
 // // // // // // // {
-// // // // // // //     let revStr = "";
-// // // // // // //     for(let i =str.length-1;i>=0;i--){
-// // // // // // //         revStr += str[i];
-// // // // // // //     }
-// // // // // // //     if(str ==revStr){
-// // // // // // //         console.log("Palindrome");
-// // // // // // //     }else{
-// // // // // // //         console.log("Not a palindrome");
-// // // // // // //     }
-// // // // // // // }
+// // // // // // //     function(){
+// // // // // // //         console.log("Hello";)
+// // // // /     }
+// // // // }();
 
-// // // // // // // isPalindrome("sir");
-// // // // // // // isPlaindrome("madam");
-// // // // // // // isPalindrome("malayam");
-
-
-// // // // // // function onemoretime(){
-// // // // // //     console.log("not undertood tell me one more time");
-
+// // // // // // // ! arro
+// // // // // // function demo(){
+// // // // // //     console.log("Hello");
 // // // // // // }
-// // // // // // // ananymous function
-// // // // // // function(){
+// // // // // // demo();
 
-// // // // // // }
+// // // // // let x=_=console.log("hi");
+// // // // //         console.log("hello");
+// // // // //         console.log("bye");
 
-// // // // // //function  expression 
-// // // // // let  x =function(){
-// // // // //     console.log("Function expression";)
-// // // // // }
+// // // // // )
 
-// // // // // !iife
-// // // // {
-// // // //     function(){
-// // // //         console.log("Hello";)
-// /     }
-// }();
+// // // // // return keyward
+// // // // function add(a,b){
+// // // //     a+b;
+// // // // }
+// // // // let x = add(5,5);
+// // // // console.log(x);
 
-// // // // ! arro
-// // // function demo(){
-// // //     console.log("Hello");
-// // // }
-// // // demo();
+// // // // let x = (a,b) =< a+b; // implicit 
+// // // // console.log(x(10,30));
 
-// // let x=_=console.log("hi");
-// //         console.log("hello");
-// //         console.log("bye");
+// // // let x = (a,b) => a+b; // explicit
+// // // console.log(x(10,30));
 
-// // )
+// // // higher order function
 
-// // return keyward
-// function add(a,b){
-//     a+b;
-// }
-// let x = add(5,5);
+// // function hof(a){
+// //     return a;
+// // };
+
+// // let x = hof(funtion(){return "This is call back function"});
+// // console.log(x);
+
+// function add(callback){
+//     return callback(5,3);
+// };
+// let x = add(function(a,b){return a+b});
 // console.log(x);
 
-// let x = (a,b) =< a+b; // implicit 
-// console.log(x(10,30));
+// t users = ["Navya", "shravan", "kumar", "bhavandla", "vishnu"]);
+//     let x = users.map(user)=>{
+//         return user;
+//     };
+//     console.log(x);
+//     ley x = users.foreach(user)=>{
+//         return (user);
+//     });
+//     console.log(x);
 
-let x = (a,b) => a+b; // explicit
-console.log(x(10,30));
+
+// // users.foreach((user)=>{
+// //     console.log(user);
+// // });
+
+// var a=10;
+// var b=20;
+// function x(){
+//     var user = "shravan";
+//     let company = "Amazon";
+//     const sal = 1234567890;
+//     console.log(user);
+//     console.log(company);
+//     console.log(sal);
+
+//     console.log(a , b);
+// }
+// x();
