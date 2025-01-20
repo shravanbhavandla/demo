7eedb9db4dc4e2a0a2521e7bb7acf8078c6bfa17
shravan bhavandla <shravanbhavandla@gmail.com>
2025-01-20 10:33:01 +0530

    msg

---
+++ webstorage.js
@@ -0,0 +1,16 @@
+//localStorage.clear();
+
+localStorage.setItem("user1" , "paru");
+localStorage.setItem("user2" , "sunny");
+localStorage.setItem("user3" , "kumar");
+localStorage.setItem("user4" , "bhavandla");
+localStorage.setItem("user5" , "kranthi");
+localStorage.setItem("user6" , "Shravan");
+
+let user6 = localStorage.getItem("user6");
+console.log(user6);
+
+localStorage.removeItem("Item3");
+
+//localstorage.clear();
+
