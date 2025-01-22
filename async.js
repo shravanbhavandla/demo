1043c210f12a3f212e091c46ea3294d7e7b34500
shravan bhavandla <shravanbhavandla@gmail.com>
2025-01-22 10:32:00 +0530

    msg

--- async.js
+++ async.js
@@ -1,30 +1,60 @@
-// console.log(10);
-// console.log(20);
-// setTimeout(()=>{
-//     console.log(30);
-// }, 5000)
-// console.log(40);
-// console.log(50);
-// setInterval(()=>{
-//     document.writeln("MRU")
-// }, 1500)
+// // console.log(10);
+// // console.log(20);
+// // setTimeout(()=>{
+// //     console.log(30);
+// // }, 5000)
+// // console.log(40);
+// // console.log(50);
+// // setInterval(()=>{
+// //     document.writeln("MRU")
+// // }, 1500)
 
-//!Promise
-let p1 = new Promise((resolve , pending) => {});
-// console.log(p1);
+// //!Promise
+// let p1 = new Promise((resolve , pending) => {});
+// // console.log(p1);
 
-let p2 = new Promise((resolve , pending) => {
-    resolve("success");
-});
-// console.log(p2);
-p2.then((response) =>
-    {
-        console.log(respone);
-    }).catch((error)=>{
-    console.log(error);
-    }).finally(()=>console.log("finally printing for both"))
+// let p2 = new Promise((resolve , pending) => {
+//     resolve("success");
+// });
+// // console.log(p2);
+// p2.then((response) =>
+//     {
+//         console.log(respone);
+//     }).catch((error)=>{
+//     console.log(error);
+//     }).finally(()=>console.log("finally printing for both"))
 
-let p3 = new Promise((resolve , pending) => {
-    reject("Failures");
-});
-// console.log(p3);
+// let p3 = new Promise((resolve , pending) => {
+//     reject("Failures");
+// });
+// // console.log(p3);
+
+//! API fetching
+
+function fetchUsers()
+{
+   let x = fetch("https://api-github.com/users");
+    //console.log(x);
+    x
+    .then((response)=>{
+    //console.log(response);
+    //console.log(response.json());
+    return Response.json().then(data=>{
+        console.log(data);
+        let store = document.getElementById("store");
+        //console.log(store);
+        data.map((user)=>{
+            //console.log(user);
+            store.innerHTML +=
+            <tr>
+                <td>${user.id}</td>
+                <td>${user.name}</td>
+                <td>${user.email}</td>
+                <td>${user.company.name}</td>
+            </tr>
+            })
+        })
+   })
+   .catch(err=>console.log(err))
+}
+fetchUsers();
