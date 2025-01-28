d9bb6555cea543fcef31c43c7a0f2407d5dfef73
shravan bhavandla <shravanbhavandla@gmail.com>
2025-01-28 20:38:34 +0530

    burger.js dragdrop.html

--- async.js
+++ async.js
@@ -1,79 +1,88 @@
-// // // // console.log(10);
-// // // // console.log(20);
-// // // // setTimeout(()=>{
-// // // //     console.log(30);
-// // // // }, 5000)
-// // // // console.log(40);
-// // // // console.log(50);
-// // // // setInterval(()=>{
-// // // //     document.writeln("MRU")
-// // // // }, 1500)
 
-// // // //!Promise
-// // // let p1 = new Promise((resolve , pending) => {});
-// // // // console.log(p1);
+// console.log(10);
+// console.log(20);
+// setTimeout(()=>{
+//     console.log(30);
+// }, 5000)
+// console.log(40);
+// console.log(50);
+// setInterval(()=>{
+//     document.writeln("MRU")
+// },1500)
 
-// // // let p2 = new Promise((resolve , pending) => {
-// // //     resolve("success");
-// // // });
-// // // // console.log(p2);
-// // // p2.then((response) =>
-// // //     {
-// // //         console.log(respone);
-// // //     }).catch((error)=>{
-// // //     console.log(error);
-// // //     }).finally(()=>console.log("finally printing for both"))
+//! Promise
+// let p1 = new Promise((resolve , reject)=>{});
+// console.log(p1);
 
-// // // let p3 = new Promise((resolve , pending) => {
-// // //     reject("Failures");
-// // // });
-// // // // console.log(p3);
+// let p2 = new Promise((resolve, reject) => {
+//     resolve("Success");
+// });
+// console.log(p2);
+// p2.then((response) => 
+//     { 
+//         console.log(response);
+//      }).catch((error)=>{
+//     console.log(error);
+// }).finally(()=>console.log("finally printing for both"))
 
-// // //! API fetching
+// let p3 = new Promise((resolve, reject) => {
+//     reject("Failures");
+// });
 
-// // function fetchUsers()
-// // {
-// //    let x = fetch("https://api-github.com/users");
-// //     //console.log(x);
-// //     x
-// //     .then((response)=>{
-// //     //console.log(response);
-// //     //console.log(response.json());
-// //     return Response.json().then(data=>{
-// //         console.log(data);
-// //         let store = document.getElementById("store");
-// //         //console.log(store);
-// //         data.map((user)=>{
-// //             //console.log(user);
-// //             store.innerHTML +=
-// //             <tr>
-// //                 <td>${user.id}</td>
-// //                 <td>${user.name}</td>
-// //                 <td>${user.email}</td>
-// //                 <td>${user.company.name}</td>
-// //             </tr>
-// //             })
-// //         })
-// //    })
-// //    .catch(err=>console.log(err))
-// // }
-// // fetchUsers();
+// // console.log(p3);
 
-// // let p = new Promise((resolve , reject)=>{
-// //     setTimeout(()=>{
-// //         resolve("Promise successfully completing");
-// //     } , 3000)
-// // })
+// p3.then((data)=>{console.log(data);})
+// .catch(error=>console.log(error))
+// .finally(()=>console.log("finally printing for both"))
 
-// // async function demo()
-// // {
-// //     console.log("start");
-// //     let x = p;
-// //     console.log(x);
 
-// //     console.log("End");
-// // }
-// // demo();
+// !API fetching
+// console.log(window);
+
+// function fetchUsers()
+// {
+//     let x = fetch("https://jsonplaceholder.typicode.com/users");
+//     // console.log(x);
+//     x
+//     .then((response)=>{
+//         // console.log(response)
+//         // console.log(response.json());
+//         return response.json().then(data=>{
+//             console.log(data);
+//             let store = document.getElementById("store");
+//             // console.log(store);
+//             data.map((user)=>{
+//                 // console.log(user);
+//                 store.innerHTML += `
+//                 <tr>
+//                 <td>${user.id}</td>
+//                 <td>${user.name}</td>
+//                 <td>${user.email}</td>
+//                 <td>${user.company.name}</td>
+//                 </tr>
+//                 `
+//             })
+//         })
+//     })
+//     .catch(err=>console.log(err))
+// }
+// fetchUsers();
+
+// let p = new Promise((resolve , reject)=>{
+//     setTimeout(()=>{
+//         resolve("Promise successfully completing");
+//     } , 3000)
+// })
+
+// async function demo()
+// {
+//     console.log("Start");
+//     let x =await p;
+//     console.log(x);
+
+//     console.log("End");
+// }
+// demo();
 
 // async function fetchUsers(){
 //     let response =await fetch("https://api.github.com/users");
@@ -82,4 +91,6 @@
 // }
 // fetchUsers();
 
-console.log()
+
+console.log(window);
+console.log(document);
--- dom1.js
+++ dom1.js
@@ -47,9 +47,24 @@ let form=document.querySelector("form");
 let username = document.getElementById("uName");
 
 let password = document.getElementById("uPass");
+let check = document.getElementById("check");
+let show = document.getElementById("show");
 
 let gen = document.getElementById("gender");
 //console.log(gen);
+
+// ! hiding and showing password content
+check.addEventListener("click",event=>{
+    //console.log(event);
+    //console.log(event.target.checked);
+    if(event.target.checked === true){
+        password.setAttribute("type" , "text");
+        show.innerText = "hide password";
+    }else{
+        password.setAttribute("type" , "password");
+        show.innerText = "show password";
+    }
+})
 
 form.addEventListener("submit", event=>{
     event.preventDefault();
